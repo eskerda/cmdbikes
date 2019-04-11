8656bd0a3395aa8f0a85a44ee43ed6638762c89d
Hugo <hugovk@users.noreply.github.com>
2019-04-11 18:25:16 +0300

    Add python_requires and classifiers for non-EOL Python

--- setup.py
+++ setup.py
@@ -33,17 +33,18 @@ setup(
         cmdbikes=cli:cli
     ''',
     keywords='Citybikes bike sharing',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: End Users/Desktop',
         'Topic :: Utilities',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
 )
